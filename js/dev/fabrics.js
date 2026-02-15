1d34d08819f66bc7a6e354aa33aaba45e753fa7b
YaroslavIl <yaroslavovich90@gmail.com>
2026-02-15 14:10:29 +0200

    fix: size fond delivery

--- js/dev/fabrics.js
+++ js/dev/fabrics.js
@@ -3,21 +3,18 @@ import { l as lightGallery } from "./lightgallery.min.js";
 import "./common.min.js";
 const KEY = "7EC452A9-0CFD441C-BD984C7C-17C8456E";
 function initFabricsGallery() {
-  console.log("🔍 Шукаю [data-fls-fabrics]...");
   const fabricsPage = document.querySelector("[data-fls-fabrics]");
-  console.log("Знайдено fabricsPage:", fabricsPage);
   if (fabricsPage) {
     const fabricsContainer = document.querySelector(".fabrics__category");
-    console.log("Знайдено fabricsContainer:", fabricsContainer);
     if (fabricsContainer) {
       lightGallery(fabricsContainer, {
         selector: ".fabric-card__img",
         licenseKey: KEY,
         speed: 500,
         download: false,
-        counter: true
+        counter: true,
+        controls: true
       });
-      console.log("✅ Галерея тканин ініціалізована");
     }
   }
 }
